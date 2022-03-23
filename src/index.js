3ea0a7183581297b0bf54498df39c74cfbb85ab1
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2022-03-23 18:54:30 +0545

    🎉 Start Blog App Frontend using react

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// External Import
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Internal Import
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Style Import
+import './style/main.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
