b3cb221cc4fec912985ac2f9b2b50125ee44f48e
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2022-03-24 10:14:32 +0545

    🎨 Design Navbar and Footer using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div className='position-relative' style={{ minHeight: '80vh' }}>
+      <div style={{ paddingBottom: '2.5rem' }}>
+        <Navbar />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,88 @@
+// External Import
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <div className='container-fluid'>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-mdb-toggle='collapse'
+          data-mdb-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <i className='fas fa-bars'></i>
+        </button>
+
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <a className='navbar-brand mt-2 mt-lg-0' href='/'>
+            My Blog
+          </a>
+          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+            <li className='nav-item'>
+              <a className='nav-link' href='/'>
+                Home
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='/'>
+                Title 1
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='/'>
+                Title 2
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className='d-flex align-items-center'>
+          <div className='dropdown'>
+            <a
+              className='dropdown-toggle d-flex align-items-center hidden-arrow'
+              href='/'
+              id='navbarDropdownMenuAvatar'
+              role='button'
+              data-mdb-toggle='dropdown'
+              aria-expanded='false'
+            >
+              <img
+                src='https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_1280.png'
+                className='rounded-circle'
+                height='25'
+                alt='Black and White Portrait of a Man'
+                loading='lazy'
+              />
+            </a>
+            <ul
+              className='dropdown-menu dropdown-menu-end'
+              aria-labelledby='navbarDropdownMenuAvatar'
+            >
+              <li>
+                <a className='dropdown-item' href='/'>
+                  My profile
+                </a>
+              </li>
+              <li>
+                <a className='dropdown-item' href='/'>
+                  Settings
+                </a>
+              </li>
+              <li>
+                <a className='dropdown-item' href='/'>
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
